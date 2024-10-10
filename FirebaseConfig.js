16b2c653cf47244f3e1ddea2d9590846058d9ecd
tonythomasndm <tony21360@iiitd.ac.in>
2024-10-10 13:34:39 +0530

    Initial Code

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,16 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAilaYYgT2dDwl-pj3VSD138bsV6A7l_WE",
     authDomain: "ip-matchmaking.firebaseapp.com",
@@ -28,4 +20,3 @@ export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
 export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
-// export const FIREBASE_ANALYTICS = getAnalytics(FIREBASE_APP);
